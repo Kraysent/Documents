7389ee17b74b2a885c58d3e2651573a4f3625284
Artyom Zaporozhets <kraysent@gmail.com>
2023-11-16 22:23:02 +0300

    added link in the headink to main page

--- frontend/src/components/heading.tsx
+++ frontend/src/components/heading.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Heading: React.FC = () => {
   return (
     <div className="heading">
-      <div className="heading-box">
-        <span className="leftheading">doc</span>
-        <span className="rightheading">archive</span>
-      </div>
+      <Link to="/">
+        <div className="heading-box">
+          <span className="leftheading">doc</span>
+          <span className="rightheading">archive</span>
+        </div>
+      </Link>
     </div>
   );
 };
